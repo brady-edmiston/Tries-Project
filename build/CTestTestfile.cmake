1c9cc5dfb52260f01d559dba03f553f1c2b3633b
Edmiston, Brady <bred9604@colorado.edu>
2024-05-03 03:03:32 +0000

    Removing build files erroneously added to repo

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jovyan/CSPB-2270_Spring_2024/Tries-Project
-# Build directory: /home/jovyan/CSPB-2270_Spring_2024/Tries-Project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
